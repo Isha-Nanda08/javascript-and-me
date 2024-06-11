0f9abf6a2416d4ab4033b8ca5623b8f02bf85539
Isha-Nanda08 <143190568+Isha-Nanda08@users.noreply.github.com>
2024-06-11 18:47:13 +0530

    object day3

--- day3_objects.js
+++ day3_objects.js
@@ -1,5 +1,29 @@
 // singleton : apne tarah ke ek hi object h baaki objects ke kai instances bn jaate h 
-// sinleton is formed by literals
+// sinleton is formed by constructor
 // objects can be declared thru literals as well as constructor
 
+//singleton
+//Object.create() // here object is being created with the help of the constructor
 
+
+// object literals (keys: value)
+
+const mySym = Symbol("key1");
+
+const Jsuser={
+    name:"Rahul",
+    age:20,
+    mySym:"myKeyone", // will not work
+    "full name":"rahul v", // cant be accessed by . operator , use square notation
+    email:"isha@gmail.com",
+    isLoggedIn:false,
+    lastLoginDays:["Monday"]
+}
+
+// how to acces the object values
+
+console.log(Jsuser.email);
+// console.log(Jsuser[email]); // wil give error as key values are read in the form of string
+
+console.log(Jsuser["email"]); // now it will work
+console.log(Jsuser["full name"]);// we cant access full name with . operator now
